8c1bb359eaf4bde154f211e44a03858ba5b0b695
ottotto1178 <ottotto1178@gmail.com>
2024-03-28 18:12:01 +0900

    password hashing

--- src/main/java/com/sr/career_cruise/controller/LoginController.java
+++ src/main/java/com/sr/career_cruise/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.sr.career_cruise.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,24 +11,48 @@ import com.sr.career_cruise.service.LoginService;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * ログイン画面Controller
+ * 
+ * @author shimokawa
+ */
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
+  /** ログイン画面Service */
   private final LoginService service;
+
+  /** PasswordEncorder */
+  private final PasswordEncoder passwordEncoder;
   
+  /**
+   * 初期表示
+   * 
+   * @param model モデル
+   * @param form 入力情報 
+   * @return 表示画面
+   */
   @GetMapping("/login")
-  public String view(Model model, LoginForm loginForm) {
+  public String view(Model model, LoginForm form) {
     return "login";
   }
 
+  /**
+   * ログイン
+   * 
+   * @param model モデル
+   * @param form 入力情報 
+   * @return 表示画面
+   */
   @PostMapping("/login")
   public String login(Model model, LoginForm form){
     var userInfo = service.searchUserByMailAdress(form.getMailAdress());
-    var isCorrectUserAuth = userInfo.isPresent() && form.getPassword().equals(userInfo.get().getPassword());
+    var isCorrectUserAuth = userInfo.isPresent() && passwordEncoder.matches(form.getPassword(), userInfo.get().getPassword());
     if (isCorrectUserAuth){
       return "redirect:/menu";
     }else{
+      // TODO エラーメッセージはプロパティファイルで管理する
       model.addAttribute("errorMsg", "ログインIDとパスワードの組み合わせが正しくありません。");
       return "login";
     }
--- src/main/java/com/sr/career_cruise/controller/MenuController.java
+++ src/main/java/com/sr/career_cruise/controller/MenuController.java
@@ -3,8 +3,20 @@ package com.sr.career_cruise.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * メニュー画面Controller
+ * 
+ * @author shimokawa
+ * 
+ */
+
 @Controller
 public class MenuController {
+  /**
+   * 初期表示
+   * 
+   * @return 表示画面
+   */
   @GetMapping("/menu")
   public String view(){
     return "menu";
