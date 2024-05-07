22d873f76a96fcf12f9f8cbc1a91e75a14986f52
EC2 Default User <ec2-user@ip-172-31-12-236.ap-northeast-1.compute.internal>
2024-05-07 07:11:40 +0000

    part23 finished

--- src/main/java/com/sr/career_cruise/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/sr/career_cruise/authentication/UserDetailsServiceImpl.java
@@ -30,9 +30,46 @@ public class UserDetailsServiceImpl implements UserDetailsService{
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
     var userInfo = repository.findById(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    
+    var accountLockedTime = userInfo,getAccountLockedTime();
+    var is AccountLocked = accountLockedTime != null && accountLockedTime.plusHours(LOCKING_TIME).isAfter(LocalDateTime.now());
+    
     return User.withUsername(userInfo.getMailAddress())
       .password(userInfo.getPassword())
       .roles("USER")
+      .disabled(userInfo.isDisabled())
+      .accountLocked(isAccountLocked  )
       .build();
+  }
+  
+  /**
+   * 認証失敗時に失敗回数を加算、ロック日時を更新
+   * 
+   * @param event イベント情報
+   */
+  @EventListener
+  public void handle(AuthenticationFailurBadCredentialEvent event){
+    var mailAddress = event.getAuthentication().getName();
+    repository.findById(mailAddress).ifPresent(userInfo -> {
+      repository.save(userInfo.incrementLoginFailurCount());
+      
+      var isReachFailurCount = userInfo.getLoginFailurCount() == LOCKING_BORDER_COUNT;
+      if(isReachFailurCount){
+        repository.save(userInfo.updateAccountLocked());
+      }
+    });
+  }
+  
+  /**
+   * 認証成功時に失敗回数をリセット
+   * 
+   * @param event イベント情報
+   */
+  @EventListener
+  public void handle(AuthenticationSuccessEvent event){
+    var mailAddress = event.getAuthentication().getName();
+    repository.findById(mailAddress).ifPresent(userInfo -> {
+      repository.save(userInfo.resetLoginFailurInfo());
+    });
   }
 }
--- src/main/java/com/sr/career_cruise/entity/UserInfo.java
+++ src/main/java/com/sr/career_cruise/entity/UserInfo.java
@@ -15,6 +15,7 @@ import lombok.Data;
 @Table(name="user_info")
 @Data
 public class UserInfo {
+  /** 氏名 */
   private String name;
 
   /** メールアドレス */
@@ -24,4 +25,37 @@ public class UserInfo {
 
   /** パスワード */
   private String password;
+  
+  /** ログイン失敗回数 */
+  @Column(name = "login_failur_count")
+  private int loginFailurCount = 0;
+  
+  /** アカウントロック日時 */
+  @Column(name = "account_locked_time")
+  private LocalDateTime accountLockedTime;
+  
+  /** 利用可不可 */
+  @Column(name = "is_disabled")
+  private boolean isDisabled;
+  
+  public UserInfo(){
+  }
+  
+  /**
+   * ログイン失敗回数を加算する
+   * 
+   * @return ログイン失敗回数が加算されたUserInfo
+   */
+  public UserInfo incrementFailurCount(){
+    return new UserInfo(name, mailaddress, password, ++loginFailurCount, accountLockedTime, isDisabled);
+  }
+  
+  /**
+   * ログイン失敗回数を加算する
+   * 
+   * @return ログイン失敗回数が加算されたUserInfo
+   */
+  public UserInfo resetLoginFailurInfo(){
+    return new UserInfo(name, mailaddress, password, 0, null, isDisabled);
+  }
 }
